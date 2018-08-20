513ecdb48dda8fbce2618e8648111fa13e5c50df
TrotzigerEmil <roadto3kmmr@gmail.com>
2018-08-20 14:30:38 +0700

    Fixed issues with older files

--- include/input.h
+++ include/input.h
@@ -1,35 +1,10 @@
 #ifndef LONGCALC_H_INPUT
 #define LONGCALC_H_INPUT
 
-#include <stdio.h>
-#include <stdlib.h>
-
 /* В заголовочном файле input.h описана функция, возвращающая вводимое с клавиатуры выражение. */
-#define BUFMULT 1024
+#define BUFMULT 1024 // начальный размер буфера чтения
 
 /* inputString: чтение строки из stdin */
-char *inputString(void)
-{
-    char *ret = malloc(BUFMULT);
-    *ret = 0;
-    unsigned bpos = 0;
-    unsigned bcursiz = BUFMULT;
-
-    char c;
-
-    while ((c = getchar()) != '\n') // enter
-    {
-	ret[bpos] = c;
-	bpos++;
-	if (bpos == bcursiz) // заполнение текущего буфера
-	{
-	    bcursiz *= 2;
-	    ret = realloc(ret, bcursiz);
-	}
-	ret[bpos] = 0;
-    }
-
-    return ret;
-}
+char *inputString(void);
 
 #endif
--- include/stack.h
+++ include/stack.h
@@ -5,9 +5,6 @@
    качестве данных строки символов, а также функции, производящие операции над сте-
    ком (инициализация, добавление на вершину, удаление с вершины, удаление стека). */
 
-#include <stdlib.h>
-#include <string.h>
-
 struct node; // элемент стека
 typedef struct node *nptr;
 
@@ -23,51 +20,21 @@ typedef struct
 } Stack;
 
 /* initStack: инициализация стека начальными значениями */
-Stack initStack(void)
-{
-    Stack ret = {NULL};
-    return ret;
-}
+Stack initStack(void);
 
 /* push: добавление новых данных на вершину стека */
-void push(char *data, Stack *S)
-{
-    StackNode *new = malloc(sizeof(StackNode));
-    new->data = malloc(strlen(data) + 1);
-    strcpy(new->data, data);
-    new->next = S->head;
-    S->head = new;
-}
+void push(char *data, Stack *S);
 
 /* pop: извлечение данных с вершины стека */
-char *pop(Stack *S)
-{
-    if (!(S->head)) return NULL;
+char *pop(Stack *S);
 
-    char *ret = malloc(strlen(S->head->data) + 1);
-    strcpy(ret, S->head->data);
-
-    nptr nexth = S->head->next;
-    free(S->head->data);
-    free(S->head);
-    S->head = nexth;
-
-    return ret;
-}
+/* peek: просмотр вершины стека без ее удаления */
+char *peek(Stack *S);
 
 /* isEmpty: возвращает 1, если стек пуст, 0 в противном случае */
-int isEmpty(Stack S)
-{
-    return !(S.head);
-}
+int isEmpty(Stack S);
 
 /* killStack: освобождение памяти, занятой под данные стека и его структуру */
-void killStack(Stack *S)
-{
-    while (!isEmpty(*S))
-    {
-	char *tmp = pop(S); // структура освобождена
-	free(tmp); // данные освобождены
-    }
-}
+void killStack(Stack *S);
+
 #endif
